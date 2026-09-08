f58c85f40b844c5795f1d5177165c5d846634ada
ghub-priv <ghubpriv@vip.sent.at>
2026-09-08 19:16:33 +0100

    Wire Post-Watch Workflow routes

--- src/config/urls.py
+++ src/config/urls.py
@@ -65,6 +65,7 @@ urlpatterns = [
     path("", include("app.smart_watched_dates_urls")),
     path("", include("app.rapid_rating_urls")),
     path("", include("app.kodi_library_urls")),
+    path("", include("app.post_watch_urls")),
     path("", include("app.urls")),
     path("", include("integrations.urls")),
     path("", include("users.urls")),
