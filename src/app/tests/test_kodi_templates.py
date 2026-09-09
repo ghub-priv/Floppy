986fcbe6f6feaf6b794d95221449f5b02ed55254
ghub-priv <ghubpriv@vip.sent.at>
2026-09-09 21:23:52 +0100

    Test Kodi play action configuration gate

--- src/app/tests/test_kodi_templates.py
+++ src/app/tests/test_kodi_templates.py
@@ -1,5 +1,10 @@
+import os
+from unittest.mock import patch
+
 from django.template.loader import get_template
 from django.test import SimpleTestCase
+
+from app.templatetags.kodi_tags import kodi_playback_available
 
 
 class KodiTemplateLoadTests(SimpleTestCase):
@@ -12,3 +17,20 @@ class KodiTemplateLoadTests(SimpleTestCase):
         ):
             with self.subTest(template=template_name):
                 self.assertIsNotNone(get_template(template_name))
+
+    def test_kodi_playback_action_hidden_without_configuration(self):
+        with patch.dict(os.environ, {"KODI_HOST": ""}, clear=False):
+            self.assertFalse(kodi_playback_available())
+
+    def test_kodi_playback_action_available_with_valid_configuration(self):
+        with patch.dict(
+            os.environ,
+            {
+                "KODI_HOST": "192.0.2.10",
+                "KODI_PORT": "8080",
+                "KODI_SCHEME": "http",
+                "KODI_TIMEOUT": "6",
+            },
+            clear=False,
+        ):
+            self.assertTrue(kodi_playback_available())
